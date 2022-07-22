28cc95ef7632107640fa7593d580da9ef58bab3f
ShadePrima <aleksandrag1984@gmail.com>
2022-07-22 16:34:35 +0300

    added page votin css

--- src/components/VotingPage/VotingPage.jsx
+++ src/components/VotingPage/VotingPage.jsx
@@ -1,9 +1,135 @@
-import React from 'react'
+import React from "react";
+
+import likeHeader from "../../assets/img/voting/like.png";
+import heartHeader from "../../assets/img/voting/heart.png";
+import disLikeHeader from "../../assets/img/voting/dislike.png";
+import searchButton from "../../assets/img/voting/search-button.png";
+import arrowLeft from "../../assets/img/voting/arrow left 1.png";
+import cat from "../../assets/img/voting/cat.png";
+
+import leftBackground from "../../assets/img/voting/Button-panel/left-background.png";
+import centrBackground from "../../assets/img/voting/Button-panel/centr-background.png";
+import rightBackground from "../../assets/img/voting/Button-panel/right-background.png";
+import likePanel from "../../assets/img/voting/Button-panel/like.png";
+import heartPanel from "../../assets/img/voting/Button-panel/heart-center.png";
+import disLikePanel from "../../assets/img/voting/Button-panel/dis-like-left.png";
+
+import styles from "./VotingPage.module.scss";
 
 const VotingPage = () => {
   return (
-    <div>VotingPage</div>
-  )
-}
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.search}>
+          <input placeholder="Search for breeds by name" />
+          <img src={searchButton} alt="Search" />
+        </div>
+        <div className={styles.buttons}>
+          <div className={styles.button}>
+            <img src={likeHeader} alt="Like" />
+          </div>
+          <div className={styles.button}>
+            <img src={heartHeader} alt="Like" />
+          </div>
+          <div className={styles.button}>
+            <img src={disLikeHeader} alt="Like" />
+          </div>
+        </div>
+      </header>
 
-export default VotingPage
+      <div className={styles.bottomSection}>
+        <section className={styles.section}>
+          <div className={styles.topButton}>
+            <div className={styles.arrowLeft}>
+              <img src={arrowLeft} alt="arrow-left" />
+            </div>
+            <div className={styles.sectionButton}>VOTING</div>
+          </div>
+          <div className={styles.content}>
+            <img className={styles.contenImgCat} src={cat} alt="Cat" />
+            <div className={styles.panelButton}>
+              <div className={styles.left}>
+                <img
+                  className={styles.backgraund1}
+                  src={leftBackground}
+                  alt="Left"
+                />
+                <img className={styles.likes} src={likePanel} alt="Like" />
+              </div>
+              <div className={styles.left}>
+                <img
+                  className={styles.backgraund2}
+                  src={centrBackground}
+                  alt="Centet"
+                />
+                <img className={styles.likes2} src={heartPanel} alt="Heart" />
+              </div>
+              <div className={styles.left}>
+                <img
+                  className={styles.backgraund3}
+                  src={rightBackground}
+                  alt="Right"
+                />
+                <img
+                  className={styles.likes3}
+                  src={disLikePanel}
+                  alt="DisLike"
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+        <footer className={styles.footer}>
+          <div className={styles.footerItems}>
+            <div className={styles.footerItem}>
+              <div className={styles.footerLeft}>
+                <div className={styles.time}>10:24</div>
+                <div className={styles.text}>
+                  Image ID: fQSunHvl8 was added to Favourites
+                </div>
+              </div>
+              <div className={styles.footerLike}>
+                <img src={likeHeader} alt="Like" />
+              </div>
+            </div>
+            <div className={styles.footerItem}>
+              <div className={styles.footerLeft}>
+                <div className={styles.time}>10:24</div>
+                <div className={styles.text}>
+                  Image ID: fQSunHvl8 was added to Favourites
+                </div>
+              </div>
+              <div className={styles.footerLike}>
+                <img src={likeHeader} alt="Like" />
+              </div>
+            </div>
+            <div className={styles.footerItem}>
+              <div className={styles.footerLeft}>
+                <div className={styles.time}>10:24</div>
+                <div className={styles.text}>
+                  Image ID: fQSunHvl8 was added to Favourites
+                </div>
+              </div>
+              <div className={styles.footerLike}>
+                <img src={likeHeader} alt="Like" />
+              </div>
+            </div>
+            <div className={styles.footerItem}>
+              <div className={styles.footerLeft}>
+                <div className={styles.time}>10:24</div>
+                <div className={styles.text}>
+                  Image ID: fQSunHvl8 was added to Favourites
+                </div>
+              </div>
+              <div className={styles.footerLike}>
+                <img src={likeHeader} alt="Like" />
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default VotingPage;
